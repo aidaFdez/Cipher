3d333aea079cc45667232b1cb1ebae63d278e8ff
aidaFdez <33495958+aidaFdez@users.noreply.github.com>
2018-06-24 20:20:49 +0200

    Changed the import
    Changed the import. It does nothing, but at least now it's shorter to type. Also, I am now trying to output the result as a popup

--- Cipher.py
+++ Cipher.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 
 abc = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K',
     'l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w', 'W',
@@ -7,6 +7,7 @@ ciph = "Here will be the output"
 
 #Uses the Caesar cipher. It shifts the letters the times given.
 def caesar ():
+    result = Toplevel()
     input = toCip.get()
     num = (caesarN.get())
     ciphered = ""
@@ -16,11 +17,13 @@ def caesar ():
             ciphered = ciphered + abc[(ind+(num*2) )%52]
         else:
             ciphered = ciphered + letter
-    ciph = ciphered
+    output = Label(result, text = ciphered)
+    output.grid()
 
 #Uses the Vigènere cipher. It uses a square table, which in essence is just applying Caesar a different number of times
 #depending on the letter of the keyword being used at the moment.
 def vigenere ():
+    result = Toplevel()
     input = toCip.get()
     keyword = vigKey.get()
     ciphered = ""
@@ -32,36 +35,37 @@ def vigenere ():
             else:
                 ciphered = ciphered + letter
             i = i+1
-    ciph = ciphered
+    output = Label(result, text = ciphered)
+    output.grid()
 
-window = tk.Tk()
+window = Tk()
 window.geometry("500x300")
 
-mPrinc = tk.Message(window, text = "Hi! Fill in the necessary data and then choose the kind of ciphering you want! \n")
+mPrinc = Message(window, text = "Hi! Fill in the necessary data and then choose the kind of ciphering you want! \n")
 mPrinc.config(width = 200)
 mPrinc.grid()
 
-msg = tk.Message(window, text = "Text to cipher ")
+msg = Message(window, text = "Text to cipher ")
 msg.config(width = 100)
 msg.grid(sticky = "w")
-toCip = tk.Entry(window)
+toCip = Entry(window)
 toCip.grid(sticky = "w", row = 1, column = 1)
 
-msgC = tk.Message(window, text = "Number (only Caesar) ")
+msgC = Message(window, text = "Number (only Caesar) ")
 msgC.grid(sticky = "w", row = 2, column = 0)
-caesarN = tk.Entry(window)
+caesarN = Entry(window)
 caesarN.grid(row = 2, column = 1)
 
-msgV = tk.Message(window, text = "Key (only Vigenere) ")
+msgV = Message(window, text = "Key (only Vigenere) ")
 msgV.grid(row = 3, sticky = "w")
-vigKey = tk.Entry(window)
+vigKey = Entry(window)
 vigKey.grid(row = 3, column = 1)
 
-res = tk.Label(window, text = ciph)
-res.grid(row = 5, column = 0)
+#res = tk.Label(window, text = ciph)
+#res.grid(row = 5, column = 0)
 
-tk.Button(window, text = "Caesar", command = caesar()).grid(row = 7)
-tk.Button(window, text = "Vigenere", command = vigenere()).grid(row = 7, column = 1)
+Button(window, text = "Caesar", command = caesar()).grid(row = 7)
+Button(window, text = "Vigenere", command = vigenere()).grid(row = 7, column = 1)
 
 
 
