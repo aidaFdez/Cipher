92d0d4bb8858d91f7deeaac7a5b0f24620087ca1
aidaFdez <33495958+aidaFdez@users.noreply.github.com>
2018-06-24 20:06:28 +0200

    Almost...
    I just need to find out how to display the result of the functions...

--- Cipher.py
+++ Cipher.py
@@ -3,9 +3,12 @@ import tkinter as tk
 abc = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K',
     'l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w', 'W',
     'x','X','y', 'Y','z', 'Z']
+ciph = "Here will be the output"
 
 #Uses the Caesar cipher. It shifts the letters the times given.
-def caesar (input, num):
+def caesar ():
+    input = toCip.get()
+    num = (caesarN.get())
     ciphered = ""
     for letter in input:
         if letter in abc:
@@ -13,11 +16,13 @@ def caesar (input, num):
             ciphered = ciphered + abc[(ind+(num*2) )%52]
         else:
             ciphered = ciphered + letter
-    return ciphered
+    ciph = ciphered
 
 #Uses the Vigènere cipher. It uses a square table, which in essence is just applying Caesar a different number of times
 #depending on the letter of the keyword being used at the moment.
-def vigenere (input, keyword):
+def vigenere ():
+    input = toCip.get()
+    keyword = vigKey.get()
     ciphered = ""
     i = 0
     while i <len(input):
@@ -27,10 +32,10 @@ def vigenere (input, keyword):
             else:
                 ciphered = ciphered + letter
             i = i+1
-    return ciphered
+    ciph = ciphered
 
 window = tk.Tk()
-window.geometry("500x200")
+window.geometry("500x300")
 
 mPrinc = tk.Message(window, text = "Hi! Fill in the necessary data and then choose the kind of ciphering you want! \n")
 mPrinc.config(width = 200)
@@ -38,19 +43,26 @@ mPrinc.grid()
 
 msg = tk.Message(window, text = "Text to cipher ")
 msg.config(width = 100)
-msg.grid()
+msg.grid(sticky = "w")
 toCip = tk.Entry(window)
 toCip.grid(sticky = "w", row = 1, column = 1)
 
 msgC = tk.Message(window, text = "Number (only Caesar) ")
-msgC.grid(row = 2, column = 0)
+msgC.grid(sticky = "w", row = 2, column = 0)
 caesarN = tk.Entry(window)
 caesarN.grid(row = 2, column = 1)
 
 msgV = tk.Message(window, text = "Key (only Vigenere) ")
-msgV.grid(row = 3)
+msgV.grid(row = 3, sticky = "w")
 vigKey = tk.Entry(window)
 vigKey.grid(row = 3, column = 1)
+
+res = tk.Label(window, text = ciph)
+res.grid(row = 5, column = 0)
+
+tk.Button(window, text = "Caesar", command = caesar()).grid(row = 7)
+tk.Button(window, text = "Vigenere", command = vigenere()).grid(row = 7, column = 1)
+
 
 
 window.mainloop()
